67ab405bcc55f492bb5a10ca71989254d30d30bd
Arthur Fiorese <arthur.herrada@gmail.com>
2026-06-02 20:28:54 -0300

    feat: adição de menu hamburguer funcional

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,64 @@
 const header = document.querySelector('header');
-
+ 
 window.addEventListener('scroll', () => {
-
+ 
     if (window.scrollY > 20) {
-        header.classList.add('rolado'); 
+        header.classList.add('rolado');
     } else {
-        header.classList.remove('rolado'); 
+        header.classList.remove('rolado');
     }
 });
-
-const btnMissao = document.getElementById("btn-missao");
-
+ 
+/* botão missão */
+ 
+const btnMissao = document.getElementById("btnMissao");
+ 
 btnMissao.addEventListener("click", function(event) {
+ 
     event.preventDefault();
-
+ 
     const elemento = document.getElementById("cor1");
-
+ 
     window.scrollTo({
         top: elemento.offsetTop - 100,
         behavior: "smooth"
     });
+ 
 });
+ 
+/* MENU */
+ 
+const menuIcone = document.getElementById("menu-icone");
+const menuOverlay = document.getElementById("menuOverlay");
+const fecharMenu = document.getElementById("fecharMenu");
+ 
+menuIcone.addEventListener("click", () => {
+    menuOverlay.classList.add("ativo");
+});
+ 
+fecharMenu.addEventListener("click", () => {
+    menuOverlay.classList.remove("ativo");
+});
+ 
+/* links */
+ 
+document.querySelectorAll(".menu-item").forEach(link => {
+ 
+    link.addEventListener("click", function(e){
+ 
+        e.preventDefault();
+ 
+        const alvo = document.querySelector(
+            this.getAttribute("href")
+        );
+ 
+        window.scrollTo({
+            top: alvo.offsetTop - 100,
+            behavior: "smooth"
+        });
+ 
+        menuOverlay.classList.remove("ativo");
+    });
+ 
+});
+ 
